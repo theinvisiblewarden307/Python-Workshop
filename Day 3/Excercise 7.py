292e20fa12e3b44b3bc375b9bdb9536961bc7d52
Ramanuj Gupta <69420768+theinvisiblewarden307@users.noreply.github.com>
2024-05-10 23:01:45 +0530

    Delete Day 3 directory

--- Day 3/Excercise 7.py
+++
@@ -1,8 +0,0 @@
-#Exercise: Dict to config:
-
-small={}
-for key,value in small.items():
-    with open('small.txt','a') as f:
-        f.write(f'{key}: {value}\n')
-with open('small.txt') as f:
-    print(f.read())
